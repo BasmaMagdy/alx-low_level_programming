5359d61b24e1b2faa0d626e5f52acc0cee73f65c
root <basma.magdy998@gmail.com>
2023-02-17 10:30:38 -0800

    modified the c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n>0){
+      printf("%d is Positive\n",n);
+    }else if(n==0){
+      printf("%d is Zero\n",n);
+    }else{
+      printf("%d is Negative\n",n);
+    }
+}
+
