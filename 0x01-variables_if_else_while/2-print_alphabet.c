4718b2d5cf5fb9db65dcd61285f22ae8e60dbc4f
root <basma.magdy998@gmail.com>
2023-02-18 05:50:20 -0800

    hello

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int d;
@@ -22,7 +20,5 @@ int main(void)
 			putchar(' ');
 		}
 	}
-	putchar('\n');
-
-	return (0);
+       	return (0);
 }
