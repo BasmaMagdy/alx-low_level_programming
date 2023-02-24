fa9c3476875f5a70ae27b3816fc7c2a482a53c2c
root <basma.magdy998@gmail.com>
2023-02-24 15:10:03 -0800

    done

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,9 +9,15 @@ int largest_number(int a, int b, int c)
     {
         largest = a;
     }
+    else if ( a > b && a > c){
+      largest = a;
+    }
     else if (b > a && a > c)
     {
         largest = b;
+    }
+    else if (b > a && b > c){
+      largest = b;
     }
     else
     {
