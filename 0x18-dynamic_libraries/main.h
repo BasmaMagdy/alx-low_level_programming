844159cef38ed75e913392617d71cdbe80f8f66b
BasmaMagdy <basma.magdy998@gmail.com>
2023-06-20 19:12:06 -0700

    go

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,3 @@
-#ifndef _main_h_
-#define _main_h_
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +19,3 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
-#endif /* _main_h_ */
