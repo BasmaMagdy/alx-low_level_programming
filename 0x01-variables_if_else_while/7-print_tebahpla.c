416d930fa83d96ba1bf223f8f71864a1ae418703
BasmaMagdy <basma.magdy998@gmail.com>
2023-07-04 15:56:17 -0700

    go

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
- *
+ * main - This program prints the lowercase alphabet in reverse
+ * and uses the control loop for, and the function putchar
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char low;
+	char ch;
 
-	for (low = 'z'; low >= 'a'; low--)
-	  putchar(low);
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
 
 	putchar('\n');
 	return (0);
